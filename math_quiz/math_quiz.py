720bffcdea84ad4c7210abf68429ec649f7416d1
Shubham Gupta <infotoshubham@gmail.com>
2023-11-11 06:29:20 +0100

    merging - merge branch code_cleanup to main

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -14,10 +14,14 @@ def function_B():
 
 def function_C(n1, n2, o):
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+':
+        a = n1 - n2
+    elif o == '-':
+        a = n1 + n2
+    else:
+        a = n1 * n2
     return p, a
+
 
 def math_quiz():
     s = 0
@@ -27,7 +31,9 @@ def math_quiz():
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = function_A(1, 10);
+        n2 = function_A(1, 5.5);
+        o = function_B()
 
         PROBLEM, ANSWER = function_C(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
@@ -41,6 +47,7 @@ def math_quiz():
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
     print(f"\nGame over! Your score is: {s}/{t_q}")
+
 
 if __name__ == "__main__":
     math_quiz()
