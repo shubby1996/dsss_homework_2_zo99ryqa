b2a6fe455d33d6dca76819167f52543e765d5ab8
Shubham Gupta <infotoshubham@gmail.com>
2023-11-13 21:27:32 +0100

    changed - name of package from mathquiz to math_quiz

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="mathquiz",
+    name="math_quiz",
     version="0.1",
     author="sgupta",
     author_email="shubham.g.gupta@fau.de",
